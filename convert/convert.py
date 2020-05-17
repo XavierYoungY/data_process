e125d0e33889393db8386770d6f9450f3ee31995
XavierYoung <xavieryangyang@gmail.com>
2020-05-17 22:29:46 +0800

    2

--- convert/convert.py
+++ convert/convert.py
@@ -14,18 +14,86 @@ category_item_id = -1
 image_id = 20180000000
 annotation_id = 0
 
-with open('ann.txt','r') as f:
-    anns=f.readlines()
-    for ann in anns:
-        ann=ann.split()
-        file_name=ann[0]
+
+def addCatItem(name):
+    global category_item_id
+    category_item = dict()
+    category_item['supercategory'] = 'none'
+    category_item_id += 1
+    category_item['id'] = category_item_id
+    category_item['name'] = name
+    coco['categories'].append(category_item)
+    category_set[name] = category_item_id
+    return category_item_id
 
 
-        bndbox = dict()
-        size = dict()
-        current_image_id = None
-        current_category_id = None
-        size['width'] = None
-        size['height'] = None
-        size['depth'] = None
-        pass
+def addImgItem(file_name, size):
+    global image_id
+    if file_name is None:
+        raise Exception('Could not find filename tag in xml file.')
+    if size['width'] is None:
+        raise Exception('Could not find width tag in xml file.')
+    if size['height'] is None:
+        raise Exception('Could not find height tag in xml file.')
+    image_id += 1
+    image_item = dict()
+    image_item['id'] = image_id
+    image_item['file_name'] = file_name
+    image_item['width'] = size['width']
+    image_item['height'] = size['height']
+    coco['images'].append(image_item)
+    image_set.add(file_name)
+    return image_id
+
+
+def addAnnoItem(object_name, image_id, category_id, bbox):
+    global annotation_id
+    annotation_item = dict()
+    annotation_item['segmentation'] = []
+    seg = []
+    # bbox[] is x,y,w,h
+    # left_top
+    seg.append(bbox[0])
+    seg.append(bbox[1])
+    # left_bottom
+    seg.append(bbox[0])
+    seg.append(bbox[1] + bbox[3])
+    # right_bottom
+    seg.append(bbox[0] + bbox[2])
+    seg.append(bbox[1] + bbox[3])
+    # right_top
+    seg.append(bbox[0] + bbox[2])
+    seg.append(bbox[1])
+
+    annotation_item['segmentation'].append(seg)
+
+    annotation_item['area'] = bbox[2] * bbox[3]
+    annotation_item['iscrowd'] = 0
+    annotation_item['ignore'] = 0
+    annotation_item['image_id'] = image_id
+    annotation_item['bbox'] = bbox
+    annotation_item['category_id'] = category_id
+    annotation_id += 1
+    annotation_item['id'] = annotation_id
+    coco['annotations'].append(annotation_item)
+
+if __name__ == "__main__":
+    with open('ann.json','r') as f:
+        anns=json.load(f)
+        for file_name in anns:
+            ann=anns[file_name]
+            size=ann['img_size']
+            size['depth']=int(3)
+            current_image_id = addImgItem(file_name, size)
+            bboxes = ann['bboxes']
+            for box in bboxes:
+                object_name=box[-1]
+                if object_name not in category_set:
+                    current_category_id = addCatItem(object_name)
+                else:
+                    current_category_id = category_set[object_name]
+                _ = list(map(float, box[:-1]))
+                bbox = list(map(int, _))
+                addAnnoItem(object_name, current_image_id, current_category_id,
+                            bbox)
+            pass
